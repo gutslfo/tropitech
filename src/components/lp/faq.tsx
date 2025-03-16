c3c88a3003af947705b07211d924b98687df36b4
Kim <thkm.grd@gmail.com>
2025-03-16 17:15:14 +0100

    .......

--- src/components/lp/faq.tsx
+++ src/components/lp/faq.tsx
@@ -6,6 +6,8 @@ import {
   } from "@/components/ui/accordion"
 import { Tilt } from "../ui/tilt"
 import Image from "next/image"
+import Link from "next/link"
+import { ExternalLink } from "lucide-react"
 
 const imgs = [
 	{id: 1, src: "/images/1.jpg"},
@@ -23,16 +25,10 @@ const imgs = [
 	return (
 	<div id="faq" className="w-11/12 md:w-2/3 mx-auto flex justify-center items-center gap-5 mb-44">
 	  <Accordion type="single" collapsible className="w-full">
-		<AccordionItem value="item-1">
-		  <AccordionTrigger className="text-xl">Est-ce facile d'accès ?</AccordionTrigger>
-		  <AccordionContent className="ml-5 text-lg my-2">
-			Oui, c'est à 3min à pied de la gare de Coppet.
-		  </AccordionContent>
-		</AccordionItem>
 		<AccordionItem value="item-2">
 		  <AccordionTrigger className="text-xl">Qu'est-ce que Tropitech ?</AccordionTrigger>
 		  <AccordionContent className="ml-5 text-lg my-2">
-			TropiTech est une expérience immersive mêlant musique électronique et scénographie tropicale dans un cadre atypique.<br/><br/>Organisée dans les Caves du Château de Coppet, cette soirée propose un voyage sonore évolutif, de la house à la techno, porté par des artistes émergents et underground.<br/><br/>L’objectif : créer une atmosphère unique où la fête prend une dimension sensorielle, entre basses vibrantes et décor exotique.
+			Tropitech est une expérience immersive mêlant musique électronique et scénographie tropicale dans un cadre atypique.<br/><br/>Organisée dans les Caves du Château de Coppet, cette soirée propose un voyage sonore évolutif, de la house à la techno, porté par des artistes émergents et underground.<br/><br/>L’objectif : créer une atmosphère unique où la fête prend une dimension sensorielle, entre basses vibrantes et décor exotique.
 		  </AccordionContent>
 		</AccordionItem>
 		<AccordionItem value="item-3">
@@ -90,8 +86,8 @@ const imgs = [
 										className='flex flex-col overflow-hidden border border-zinc-950/10 bg-white dark:border-zinc-50/10 dark:bg-zinc-900'
 									>
 										<Image
-										width={300}
-										height={300}
+										width={1000}
+										height={1000}
 										src={img.src}
 										alt='Image Tropitech'
 										className='h-80 w-full object-cover'
@@ -100,6 +96,26 @@ const imgs = [
 									</Tilt>
 								))}
 							</div>
+		  </AccordionContent>
+		</AccordionItem>
+		<AccordionItem value="item-1">
+		  <AccordionTrigger className="text-xl">Est-ce facile d'accès ?</AccordionTrigger>
+		  <AccordionContent className="ml-5 text-lg my-2">
+			Oui, c'est à 3min à pied de la gare de Coppet.<br/><br/>Lien Google Maps :<br/><span className="block h-2" /><Link href={"https://maps.app.goo.gl/7D6145TF1Q7NVCnB6"} className="underline underline-offset-2 flex justify-start items-center gap-2 duration-150 hover:text-foreground/80"><ExternalLink className="size-5" />Caves du Château, Rue du Greny</Link>
+		  </AccordionContent>
+		</AccordionItem>
+		<AccordionItem value="item-4">
+		  <AccordionTrigger className="text-xl">Où retrouver les artistes ?</AccordionTrigger>
+		  <AccordionContent className="ml-5 text-lg my-2">
+			Edo b2b Lutchi :<br/><span className="block h-2" /><Link href={"https://soundcloud.com/edoardo-failla-545458741/lets-just-grab-one-beer?si=673fc458937547f6970f6526dd820328&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"} className="underline underline-offset-2 flex justify-start items-center gap-2 duration-150 hover:text-foreground/80"><ExternalLink className="size-5" />Soundcloud</Link>
+			<br/>
+			EREIB :<br/><span className="block h-2" /><Link href={"https://soundcloud.com/lakesidebasement/basement-podcast-x-introspective-ereib?in=ereib/sets/podcasts&si=62b33d65021b4055a126d7a7915a0055&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"} className="underline underline-offset-2 flex justify-start items-center gap-2 duration-150 hover:text-foreground/80"><ExternalLink className="size-5" />Soundcloud</Link>
+			<br/>
+			Cadence :<br/><span className="block h-2" /><Link href={"https://soundcloud.com/mitsu2000/mitsucast-067-cadence?in=cadence_live/sets/mixes&si=04948642d6804029a927ce8e691f687d&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"} className="underline underline-offset-2 flex justify-start items-center gap-2 duration-150 hover:text-foreground/80"><ExternalLink className="size-5" />Soundcloud</Link>
+		    <br/>
+			Götz :<br/><span className="block h-2" /><Link href={"https://soundcloud.com/gotzzzz/immersion?si=3b328cbb8c8541dea670dffda265ad90&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"} className="underline underline-offset-2 flex justify-start items-center gap-2 duration-150 hover:text-foreground/80"><ExternalLink className="size-5" />Soundcloud</Link>
+		    <br/>
+			Cadence :<br/><span className="block h-2" /><Link href={"https://soundcloud.com/user-875894149/venuss-1-home-set_1024?si=cdb8cc00a0da417eb3e03d3daf4bdc1e&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing"} className="underline underline-offset-2 flex justify-start items-center gap-2 duration-150 hover:text-foreground/80"><ExternalLink className="size-5" />Soundcloud</Link>
 		  </AccordionContent>
 		</AccordionItem>
 	  </Accordion>
