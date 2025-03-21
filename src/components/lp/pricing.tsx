1e680244238c4486bef6e2ebb65b3fae6b254f99
gutslfo <128405176+gutslfo@users.noreply.github.com>
2025-03-21 19:28:13 +0100

    Consentement image

--- src/components/lp/pricing.tsx
+++ src/components/lp/pricing.tsx
@@ -7,19 +7,11 @@ import {
 	CardTitle,
   } from "@/components/ui/card"
 import { Button } from "../ui/button"
-import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from "@/components/ui/dialog"
+import Link from "next/link"
 
 export default function PricingCard() {
 	return (
 		<div id="tarifs" className="w-11/12 md:w-2/3 mx-auto grid grid-cols-1 md:grid-cols-3 gap-5 mb-20">
-		<Dialog>
 			<Card className="min-w-44">
 				<CardHeader>
 					<CardTitle>Early Bird</CardTitle>
@@ -29,9 +21,9 @@ export default function PricingCard() {
 					<CardTitle className="text-4xl mb-2">10.-</CardTitle>
 				</CardContent>
 				<CardFooter>
-					<DialogTrigger asChild>
+					<Link href="/payment" className="w-full">
 						<Button variant={"outline"} className="mt-2 w-full">Acheter maintenant</Button>
-					</DialogTrigger>
+					</Link>
 				</CardFooter>
 			</Card>
 			<Card className="min-w-44">
@@ -43,9 +35,9 @@ export default function PricingCard() {
 					<CardTitle className="text-4xl mb-2">15.-</CardTitle>
 				</CardContent>
 				<CardFooter>
-					<DialogTrigger asChild>
+					<Link href="/payment" className="w-full">
 						<Button variant={"outline"} className="mt-2 w-full">Acheter maintenant</Button>
-					</DialogTrigger>
+					</Link>
 				</CardFooter>
 			</Card>
 			<Card className="min-w-44">
@@ -57,24 +49,11 @@ export default function PricingCard() {
 					<CardTitle className="text-4xl mb-2">18.-</CardTitle>
 				</CardContent>
 				<CardFooter className="w-full self-end">
-					<DialogTrigger asChild>
+					<Link href="/payment" className="w-full">
 						<Button variant={"outline"} className="mt-2 w-full">Acheter maintenant</Button>
-					</DialogTrigger>
+					</Link>
 				</CardFooter>
 			</Card>
-			<DialogContent>
-				<DialogHeader>
-				<DialogTitle>Droit à l'image</DialogTitle>
-				<DialogDescription>
-					Vous devez accepter le droit à l'image avant de pouvoir acheter
-				</DialogDescription>
-				</DialogHeader>
-				<h1>J'accepte que mon image et ma voix soient utilisées sous forme de photos, vidéos et autres contenus enregistrés pendant l'événement.<br/><br/>Ces contenus pourront être diffusés à des fins de promotion et de communication sur des supports comme les réseaux sociaux, sites internet, etc.<br/><br/>Pour plus d’informations, consultez nos conditions d’utilisation et politique de confidentialité.</h1>
-				<div className="w-full flex justify-end items-center mt-3">
-					<Button>J'accepte</Button>
-				</div>
-			</DialogContent>
-		</Dialog>
 		</div>
 	)
 }
